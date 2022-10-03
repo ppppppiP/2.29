770bc9127e684dfd1b4cfdd8286ba9d12f173264
sarpi <sarpi@SARPINSKIY>
2022-10-03 21:21:04 +0300

    Добавьте файлы проекта.

---
+++ 2.29/Program.cs
@@ -0,0 +1,11 @@
+﻿// задание 2.21
+
+
+Console.WriteLine("Введите число n  ");
+int n = int.Parse(Console.ReadLine());
+
+int x = n % 10 + n / 100 * 10 + n % 100 / 10 * 100;
+
+
+Console.WriteLine(x);
+
